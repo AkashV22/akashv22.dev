2c89a5398f7a0cd77ac9ae62cbecdf631716630d
AkashV22 <iakashv22@gmail.com>
2024-02-05 08:09:49 +0000

    Added index and home page HTML templates.

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"bytes"
 	"log/slog"
 	"net/http"
 	"os"
+	"text/template"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("templates/index.html", "templates/pages/home.html"))
+
+		var buf bytes.Buffer
+
+		if err := tmpl.Execute(&buf, nil); err != nil {
+			slog.Error("Error rendering HTML template.", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		buf.WriteTo(w)
+	})
+
 	if err := http.ListenAndServe(":8080", mux); err != nil {
-		slog.Error("Failed to start HTTP server.", err)
+		slog.Error("Error starting HTTP server.", err)
 		os.Exit(1)
 	}
 }
